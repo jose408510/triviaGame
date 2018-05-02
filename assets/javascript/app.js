7490977f9340cb07aa04293e872987622fc1ea2a
Jose Castellanos <josecastellanos@calvisitor-10-105-194-96.calvisitor.1918.berkeley.edu>
2018-05-02 09:59:01 -0700

    button fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,41 @@
-var score = 0;
-var correct =  0;
-var inCorrect= 0; 
-var image = '../images/loading.gif'
-
+var myQuestion = [
+    {// 1st
+    question: "What year was the first iPhone released?",
+    choices:["1993","Yesterday","2007","2018"],
+    answer: 3
+}, 
+    {// 2nd question
+    question: "Who is the Main character in the Dark Knight?",
+    choices: ["Luke Skywalker","Batman","The Joker", "Obama"],
+    answer: 2
+},
+    {// 3rd
+    question:"Who won the NBA championship last year?",
+    choices:["The Warriors","The Lakers","Barcelona","The Cavs"],
+    answer: 1 
+},
+    {// 4th
+    question:"Who is the CEO of Facebook ?",
+    choices:["Jerry Brown","Mark Zuckaberg","Ironman","Oprah"],
+    answer: 2
+},
+    {// 5th
+    question:"Which of the four Cars is the fastest in the quarter mile?",
+    choices:["Model S P100D","Dodge SRT 8","Prius","Rimac"],
+    answer: 4
+    }];
+ 
 // the first click 
 $('#theClick').on("click", function(){
  alert("yo");
  $('#theClick').remove();
- $('mainOne').html(image);
 })  
 
 
-// function firstQuestion(){
-//     $("#mainOne").html("hello" );
-//     // ("<img src='../images/loading.gif' width='200px'/>");
-// }
+$('#choices').remove("buttons");
+
 
 
 // ../images/retina_wood.png
 // /assets/images/loading.gif
+
